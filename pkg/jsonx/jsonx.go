9999b09b367e2924ea9327c2b7cbd248f851bd3e
Roman Sheptytsky <202754781+mascotmascot1@users.noreply.github.com>
2026-09-10 02:15:59 +0300

    Enhance UnmarshalJSON with parameterized receiver comment
    Add comment explaining parameterized receiver in UnmarshalJSON method.

--- pkg/jsonx/jsonx.go
+++ pkg/jsonx/jsonx.go
@@ -9,6 +9,8 @@ type Field[T any] struct {
 	Valid bool
 }
 
+// Метод с параметризованным ресивером (method with a parameterized receiver). Это не type inference (вывод типов),
+// это явное указание type-параметра для метода, чтобы он привязался к дженерик-структуре.
 func (f *Field[T]) UnmarshalJSON(data []byte) error {
 	f.Valid = true
 
